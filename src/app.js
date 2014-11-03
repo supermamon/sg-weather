c7d18f96094b1687e18ff0ad8f46099a4ba73e7c
Raymond <monvelasquez@gmail.com>
2014-11-03 13:47:16 +0800

    Complete Info (almost)
    - Added min/max temp
    - Added units
    - Added sunset/sunrise
    - Handle API error

--- src/app.js
+++ src/app.js
@@ -5,27 +5,40 @@
  */
 
 var UI = require('ui');
-var Vector2 = require('vector2');
 var ajax = require('ajax');
+var Vibe = require('ui/vibe');
 
 // Construct URL
 var cityName = 'Singapore';
+//var URL = 'http://api.openweathermap.org/data/2.5/find?q=' + cityName;
 var URL = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName;
-
 
 var main = new UI.Card({
 	title: 'SG Weather',
-	icon: 'images/info.png',
+	//icon: 'images/info.png',
 	subtitle: 'Fetching...',
-	body: ':)'
+	body: ''//,  scrollable:true
 });
 
 main.show();
 refreshWeather();
 
+function formatTime(utc) {
+  var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
+  d.setUTCSeconds(utc);
+  var sTime = '';
+  if (d.getHours() < 10) {sTime += '0'+d.getHours();}
+  else {sTime += d.getHours();}
+  sTime+=':';
+  if (d.getMinutes() < 10) {sTime += '0'+d.getMinutes();}
+  else {sTime += d.getMinutes();}
+  
+  return sTime;
+}
+
 function refreshWeather() {
 	main.subtitle('Fetching...');
-	main.body(':)');
+	main.body('');
 	
 	ajax(
 		{
@@ -35,18 +48,46 @@ function refreshWeather() {
 		function(data) {
 			// Success!
 			console.log('Successfully fetched weather data!');
-
+      console.log(JSON.stringify(data));
+      
+      if (data.cod != 200) {
+        main.subtitle('Failed.');
+        main.body(data.message);
+        return;
+      }
+      
 			// Extract data
-			var location = data.name;
-			var temperature = Math.round(data.main.temp - 273.15) + 'C';
+			//var location = data.name;
 
+      //data = data.list[0];
+      var summary = data.weather[0].description;
 			// Always upper-case first letter of description
-			var description = data.weather[0].description;
-			description = description.charAt(0).toUpperCase() + description.substring(1);
+      summary = summary.charAt(0).toUpperCase() + summary.substring(1);
+      
+			var temperature = Math.round(data.main.temp - 273.15) + 'C';
+      temperature += ' (' + Math.round(data.main.temp_min - 273.15) + '';
+      temperature += '/'  + Math.round(data.main.temp_max - 273.15) + ')';
+      
+      var utcSeconds = data.sys.sunrise;
+      var sunrise = new Date(0); // The 0 there is the key, which sets the date to the epoch
+      sunrise.setUTCSeconds(utcSeconds);
+
+      utcSeconds = data.sys.sunset;
+      var sunset = new Date(0); // The 0 there is the key, which sets the date to the epoch
+      sunset.setUTCSeconds(utcSeconds);
+      
+      var description = '';
+      description += summary;
+      description += '\nTemp: ' + temperature;
+      description += '\nHumidity: '+data.main.humidity + ' %';
+      description += '\nPressure: '+data.main.pressure+' hpa';
+      description += "\nWind: "+data.wind.speed+'mph '+data.wind.deg+' deg';
+      description += '\nSun: ' + formatTime(data.sys.sunrise) + ' / ' + formatTime(data.sys.sunset);
 			
 			// Show to user
-			main.subtitle(temperature);
+			main.subtitle('');
 			main.body(description);
+      Vibe.vibrate('short');
 		},
 		function(error) {
 			// Failure!
@@ -57,50 +98,7 @@ function refreshWeather() {
 
 }
 
-
-
 /* Events */
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }]
-    }]
-  });
-  menu.on('select', 
-  function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  //menu.show();
-});
-
 main.on('click', 'select', function(e) {
   refreshWeather();	
-  /*
-  var wind = new UI.Window();
-  var textfield = new UI.Text({
-    position: new Vector2(0, 50),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
-    textAlign: 'center'
-  });
-  wind.add(textfield);
-  wind.show();
-  */
-});
-
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
 });
